ebe154a1165dc5953ede1af6531adbd3c06ff226
dominik lalik 1tib <106179525+Cyrkiel17@users.noreply.github.com>
2025-10-09 03:00:47 +0200

    Add files via upload

--- funkcja strzałkowa.php
+++ funkcja strzałkowa.php
@@ -1,10 +1,6 @@
 <?php
 
-$filename = 'bitnami.css';
 
-if (file_exists($filename)){
-    echo "the file $filename exists";
-} else {
-    echo "the file $filename does not exist"
-}
+$y = 1;
+$fn1 = fn($x) => $x + $y;
 ?>
